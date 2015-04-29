d328cc3cbb6df48447b4fcb191cff9a512f39dde
willreddin <willreddin88@gmail.com>
2015-04-29 15:41:03 +0100

    chapter 4

---
+++ ask-name.rb
@@ -0,0 +1,11 @@
+puts 'Hello there, and what\'s your first name?'
+name = gets.chomp
+
+puts 'okay and your second?'
+surname = gets.chomp
+
+puts 'great what\'s your middle name?'
+middle_name = gets.chomp
+
+puts 'okay so your name is...'
+puts name + ' ' + middle_name + ' ' + surname
