f39bd0e8418a7b0d6855462dc43203cbef537a6b
willreddin <willreddin88@gmail.com>
2015-05-03 12:20:09 +0100

    start arrays chapter

---
+++ love-lang.rb
@@ -0,0 +1,9 @@
+languages = ['English', 'German', 'Swedish']
+
+languages.each do |lang|
+	puts 'I love ' + lang + '!'
+	puts 'Don\'t you?'
+end
+
+puts 'And lets hear it for C++!'
+puts '...'
---
+++ write-words.rb
@@ -0,0 +1,10 @@
+words = []
+
+input = gets.chomp
+
+input.join
+
+words.push input
+
+puts words.sort
+
